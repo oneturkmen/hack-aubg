7f7debb811d028f7be84789de577e8cd151bcc3b
pidor <lojukaxek@hotmailpro.info>
2019-02-16 23:32:28 +0300

    GUI and demo updated

--- service_example/service_example/Program.cs
+++ service_example/service_example/Program.cs
@@ -13,9 +13,11 @@ namespace service_example
         {
             var exitCode = HostFactory.Run(x =>
             {
+                //double t = Double.Parse(args[0]);
+                double t = 10000;
                 x.Service<Servc>(s =>
                 {
-                s.ConstructUsing(Servc => new Servc());
+                s.ConstructUsing(Servc => new Servc(t));
                 s.WhenStarted(Servc => Servc.Start());
                 s.WhenStopped(Servc => Servc.Stop());
 
@@ -24,6 +26,8 @@ namespace service_example
                 x.SetServiceName("Procrast");
                 x.SetDisplayName("Antiproc");
                 x.SetDescription("Simple description");
+
+                x.AddCommandLineDefinition("t",v=>t = Double.Parse(v));
             });
             int exit = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
             Environment.ExitCode = exit;
