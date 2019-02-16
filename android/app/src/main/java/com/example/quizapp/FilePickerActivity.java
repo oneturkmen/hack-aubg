a089e6d1ca0787a887d4a925b3abe311a9cc539f
oneturkmen <oneturkmen@gmail.com>
2019-02-17 00:24:19 +0200

    WIP: trying to figure out filepath

--- android/app/src/main/java/com/example/quizapp/FilePickerActivity.java
+++ android/app/src/main/java/com/example/quizapp/FilePickerActivity.java
@@ -1,13 +1,69 @@
 package com.example.quizapp;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AndroidRuntimeException;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class FilePickerActivity extends AppCompatActivity {
+
+    TextView filePathShow;
+    Button btnFilePicker;
+    Intent filePathIntent;
+    List<Question> questions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_picker);
+
+        filePathShow = (TextView) findViewById(R.id.txt_filePicker);
+        btnFilePicker = (Button) findViewById(R.id.btn_filePicker);
+
+        btnFilePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                filePathIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                filePathIntent.setType("*/*");
+                startActivityForResult(filePathIntent, 10);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 10: {
+                if (resultCode == RESULT_OK) {
+                    String path = data.getData().getPath();
+                    filePathShow.setText(path);
+                    try {
+                        questions = XmlParser.parse(path);
+
+                        for (Question question : questions) {
+                            question.getAllInfo();
+                            System.out.println();
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                break;
+            }
+        }
+    }
+
+    public String getFilePathString() {
+        return this.filePathShow.getText().toString();
     }
 }
--- android/app/src/main/java/com/example/quizapp/MainActivity.java
+++ android/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 alarm.cancelAlarm(getApplicationContext());
             }
         } );
-        
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
