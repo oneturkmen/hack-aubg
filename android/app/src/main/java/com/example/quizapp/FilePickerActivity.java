6caa420ab77dba5ff23c588178a87cf71aa76c97
oneturkmen <oneturkmen@gmail.com>
2019-02-17 02:32:04 +0200

    WIP: Would not work, changing dir to QR code

--- android/app/src/main/java/com/example/quizapp/FilePickerActivity.java
+++ android/app/src/main/java/com/example/quizapp/FilePickerActivity.java
@@ -1,6 +1,15 @@
 package com.example.quizapp;
 
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +17,14 @@ import android.util.AndroidRuntimeException;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileSystems;
 import java.util.List;
 
 public class FilePickerActivity extends AppCompatActivity {
@@ -32,6 +48,7 @@ public class FilePickerActivity extends AppCompatActivity {
             public void onClick(View view) {
                 filePathIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 filePathIntent.setType("*/*");
+                filePathIntent.addCategory(Intent.CATEGORY_OPENABLE);
                 startActivityForResult(filePathIntent, 10);
             }
         });
@@ -40,22 +57,64 @@ public class FilePickerActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        String filename;
         switch (requestCode) {
             case 10: {
                 if (resultCode == RESULT_OK) {
-                    String path = data.getData().getPath();
-                    filePathShow.setText(path);
+                    Uri uri = data.getData();
+
                     try {
-                        questions = XmlParser.parse(path);
-
-                        for (Question question : questions) {
-                            question.getAllInfo();
-                            System.out.println();
+                        String mimeType = getContentResolver().getType(uri);
+                        if (mimeType == null) {
+                            String path = getPath(this, uri);
+                            if (path == null) {
+                                filename = this.getName(uri.toString());
+                            } else {
+                                File file = new File(path);
+                                filename = file.getName();
+                            }
+                        } else {
+                            Uri returnUri = data.getData();
+                            Cursor returnCursor = getContentResolver().query(returnUri, null, null, null, null);
+                            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                            int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+                            returnCursor.moveToFirst();
+                            filename = returnCursor.getString(nameIndex);
+                            String size = Long.toString(returnCursor.getLong(sizeIndex));
                         }
+                        File fileSave = getExternalFilesDir(null);
+                        String sourcePath = getExternalFilesDir(null).toString();
+                        try {
+                            // copyFileStream(new File(sourcePath + "/" + filename), uri,this);
+                            filePathShow.setText("content:/" + sourcePath + "/" + filename);
+                            System.out.println(new File("").getAbsolutePath());
+                            questions = XmlParser.parse(sourcePath + "/" + filename);
 
+                            for (Question question : questions) {
+                                question.getAllInfo();
+                                System.out.println();
+                            }
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     } catch (Exception e) {
-                        e.printStackTrace();
+
                     }
+
+                    // filePathShow.setText(path);
+//                    try {
+//                        questions = XmlParser.parse(path);
+//
+//                        for (Question question : questions) {
+//                            question.getAllInfo();
+//                            System.out.println();
+//                        }
+//
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
 
                 }
                 break;
@@ -66,4 +125,144 @@ public class FilePickerActivity extends AppCompatActivity {
     public String getFilePathString() {
         return this.filePathShow.getText().toString();
     }
+
+    private void copyFileStream(File dest, Uri uri, Context context)
+            throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = context.getContentResolver().openInputStream(uri);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            is.close();
+            os.close();
+        }
+    }
+
+
+
+    public static String getPath(Context context, Uri uri) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else
+            if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {split[1]};
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = { column };
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+
+    /** The output will be the same irrespective of the machine that the code is running on.
+     *
+     * @param filename  the filename to query, null returns null
+     * @return the name of the file without the path, or an empty string if none exists
+     */
+    public static String getName(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        int lastUnixPos = filename.lastIndexOf('/');
+        int lastWindowsPos = filename.lastIndexOf('\\');
+        return filename.substring(Math.max(lastUnixPos, lastWindowsPos) + 1);
+    }
+
 }
