cae6ef763d64d6d0cad2615988afbfcc3e5251c5
oneturkmen <oneturkmen@gmail.com>
2019-02-17 11:17:44 +0200

    Greetings styles

--- android/app/src/main/java/com/example/quizapp/WebViewer.java
+++ android/app/src/main/java/com/example/quizapp/WebViewer.java
@@ -1,22 +1,36 @@
 package com.example.quizapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class WebView extends AppCompatActivity {
+public class WebView extends Activity {
     Button btn;
     EditText editText;
+    WebView webview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://www.google.com");
+
+
         openWebPage("https://dry-cliffs-55720.herokuapp.com/");
+
+
         btn = (Button) findViewById(R.id.button_1);
         editText = (EditText) findViewById(R.id.edit_text_1);
     }
